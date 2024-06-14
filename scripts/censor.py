f1600b74d55c65a8698f1db665ec8447f03b5eb7
bosima <bosima@126.com>
2024-06-14 19:20:04 +0800

    Update censor.py
    use new cheker

--- scripts/censor.py
+++ scripts/censor.py
@@ -1,14 +1,19 @@
 import torch
-from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
+import numpy as np
+from scripts.safety_checker import StableDiffusionSafetyChecker
 from transformers import AutoFeatureExtractor
 from PIL import Image
+from diffusers.utils import logging
+
 
 from modules import scripts, shared
 
 safety_model_id = "CompVis/stable-diffusion-safety-checker"
 safety_feature_extractor = None
 safety_checker = None
+safety_checker_adj = -0.2
 
+warning_image = os.path.join("extensions", "stable-diffusion-webui-nsfw-censor", "warning", "warning.png")
 
 def numpy_to_pil(images):
     """
@@ -31,19 +36,36 @@ def check_safety(x_image):
         safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id)
 
     safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
-    x_checked_image, has_nsfw_concept = safety_checker(images=x_image, clip_input=safety_checker_input.pixel_values)
+    x_checked_image, has_nsfw_concept = safety_checker(
+        images=x_image,
+        clip_input=safety_checker_input.pixel_values,
+        safety_checker_adj=safety_checker_adj,
+    )
 
     return x_checked_image, has_nsfw_concept
 
-
-def censor_batch(x):
+def censor_batch(x, safety_checker_adj: float):
     x_samples_ddim_numpy = x.cpu().permute(0, 2, 3, 1).numpy()
-    x_checked_image, has_nsfw_concept = check_safety(x_samples_ddim_numpy)
+    x_checked_image, has_nsfw_concept = check_safety(x_samples_ddim_numpy, safety_checker_adj)
     x = torch.from_numpy(x_checked_image).permute(0, 3, 1, 2)
 
+    index = 0
+    for unsafe_value in has_nsfw_concept:
+        try:
+            if unsafe_value is True:
+                hwc = x.shape
+                y = Image.open(warning_image).convert("RGB").resize((hwc[3], hwc[2]))
+                y = (np.array(y) / 255.0).astype("float32")
+                y = torch.from_numpy(y)
+                y = torch.unsqueeze(y, 0).permute(0, 3, 1, 2)
+                x[index] = y
+            index += 1
+        except Exception as e:
+            logger.warning(e)
+            index += 1
+
     return x
-
-
+    
 class NsfwCheckScript(scripts.Script):
     def title(self):
         return "NSFW check"
@@ -53,4 +75,4 @@ class NsfwCheckScript(scripts.Script):
 
     def postprocess_batch(self, p, *args, **kwargs):
         images = kwargs['images']
-        images[:] = censor_batch(images)[:]
+        images[:] = censor_batch(images, safety_checker_adj)[:]
